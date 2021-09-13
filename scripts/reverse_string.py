ddb524d4a600fef1b1ea42876bace8e900aec010
Dylan Tintenfich <tintenfichdylan@gmail.com>
2021-09-12 23:23:53 -0300

    Add reverse2 function

--- scripts/reverse_string.py
+++ scripts/reverse_string.py
@@ -3,3 +3,10 @@ def reverse1(string):
     for character in string:
         reversed_string = character + reversed_string
     return reversed_string
+
+
+def reverse2(string):
+    reversed_string = ""
+    for i in range(len(string) - 1, -1, -1):
+        reversed_string += string[i]
+    return reversed_string
