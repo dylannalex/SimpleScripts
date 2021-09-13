3b4abb8af7738a5324681e89c72f769ec3c477d6
Dylan Tintenfich <tintenfichdylan@gmail.com>
2021-09-12 23:37:23 -0300

    Complete roots function

--- scripts/roots.py
+++ scripts/roots.py
@@ -1,3 +1,8 @@
+from math import sqrt
+
+
 def roots(a, b, c):
     """a*x**2 + b*x + c = 0"""
-    pass
+    root = sqrt(b ** 2 - 4 * a * c)
+    denominator = 2 * a
+    return (-b + root) / denominator, (-b - root) / denominator
