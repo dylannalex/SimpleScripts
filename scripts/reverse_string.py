157255cc002c4f432912dd222145eabb1e0f0135
Dylan Tintenfich <tintenfichdylan@gmail.com>
2021-09-12 23:25:10 -0300

    Add reverse3 function

--- scripts/reverse_string.py
+++ scripts/reverse_string.py
@@ -10,3 +10,13 @@ def reverse2(string):
     for i in range(len(string) - 1, -1, -1):
         reversed_string += string[i]
     return reversed_string
+
+
+def reverse3(string):
+    return string[::-1]
+
+
+string = "Hello world!"
+print(reverse1(string))
+print(reverse2(string))
+print(reverse3(string))
