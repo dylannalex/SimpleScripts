460f6abebd7d98e5cd133b4e3870819a142a82a1
Dylan Tintenfich <tintenfichdylan@gmail.com>
2021-09-13 16:28:29 -0300

    Add scale operation

--- scripts/vector_operations.py
+++ scripts/vector_operations.py
@@ -10,3 +10,7 @@ def dot_product(v1, v2):
         raise ValueError("Vectors must have the same dimension")
 
     return sum([v1[i] * v2[i] for i in range(len(v1))])
+
+
+def scale(v: list, k: float):
+    return [k * i for i in v]
