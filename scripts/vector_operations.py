c071b4f2e4b2750799e1e2486af25c4e45627417
Dylan Tintenfich <tintenfichdylan@gmail.com>
2021-09-12 23:53:26 -0300

    Add dot product

--- scripts/vector_operations.py
+++ scripts/vector_operations.py
@@ -3,3 +3,10 @@ def sum_vectors(v1, v2):
         raise ValueError("Vectors must have the same dimension")
 
     return [v1[i] + v2[i] for i in range(len(v1))]
+
+
+def dot_product(v1, v2):
+    if len(v1) != len(v2):
+        raise ValueError("Vectors must have the same dimension")
+
+    return sum([v1[i] * v2[i] for i in range(len(v1))])
